27a2d6cdf877c0ae318116ed3ef5bce067e1f237
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-05 06:33:58 +0200

    added the datetime module on app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
