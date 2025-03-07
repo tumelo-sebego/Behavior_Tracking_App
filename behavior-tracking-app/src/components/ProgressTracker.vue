bb5e641534caaf25a8a1a91aeafa7480421da5fd
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-07 15:54:38 +0200

    updated the ProgressTracker.vue file to show Progress

---
+++ behavior-tracking-app/src/components/ProgressTracker.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Daily Progress: {{ progress }}%</h2>
+  </div>
+</template>
+
+<script>
+import { useTaskStore } from "@/store/tasks";
+import { onMounted } from "vue";
+
+export default {
+  setup() {
+    const taskStore = useTaskStore();
+
+    onMounted(() => {
+      taskStore.fetchTasks();
+    });
+
+    return { progress: taskStore.progress };
+  },
+};
+</script>
