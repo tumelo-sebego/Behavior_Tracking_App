2e46bb54814b7bfd0d3e5d99ab6d76075f2e39ec
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-07 20:30:27 +0200

    added code into Home.vue view

---
+++ behavior-tracking-app/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome to the Behavior Tracking App</h1>
+    <p>Track your daily tasks and progress here.</p>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/register">Register</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
