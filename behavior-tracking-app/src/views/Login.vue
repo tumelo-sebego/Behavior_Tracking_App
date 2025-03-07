1af52d731772943af19a89b056cb9b35027df74e
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-07 20:32:52 +0200

    added code into Login.vue view

---
+++ behavior-tracking-app/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <label>Email:</label>
+      <input type="email" v-model="email" required />
+
+      <label>Password:</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log("Logging in:", this.email, this.password);
+    },
+  },
+};
+</script>
