24307641e051529753aca5006af31e143d824ec4
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-07 20:35:38 +0200

    modified code into Register.vue view

---
+++ behavior-tracking-app/src/views/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Register</h2>
+    <form @submit.prevent="register">
+      <label>Email:</label>
+      <input type="email" v-model="email" required />
+
+      <label>Password:</label>
+      <input type="password" v-model="password" required />
+
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log("Registering:", this.email, this.password);
+    },
+  },
+};
+</script>
