81064d290e280def5e0827a8a35dca11fdb7941e
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-11 17:46:33 +0200

    Added the new route for the Add Task page

--- behavior-tracking-app/src/router/index.js
+++ behavior-tracking-app/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import AddTask from "@/views/AddTask.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/login", component: Login },
   { path: "/register", component: Register },
+  { path: "/add-task", component: AddTask },
 ];
 
 const router = createRouter({
