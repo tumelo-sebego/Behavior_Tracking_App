fbc71418478023ae1bda4fc3aa4cdd6c4bd3e833
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-11 15:26:47 +0200

    modified structure of api.js

--- behavior-tracking-app/src/api.js
+++ behavior-tracking-app/src/api.js
@@ -1,10 +1,20 @@
 import axios from "axios";
 
-const API_URL = " http://localhost:8080/";
-
-export const api = axios.create({
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+const API = axios.create({
+  baseURL: "http://127.0.0.1:5000", // Flask backend URL
+  headers: { "Content-Type": "application/json" },
 });
+
+// User Authentication
+export const login = (email, password) =>
+  API.post("/auth/login", { email, password });
+export const register = (email, password) =>
+  API.post("/auth/register", { email, password });
+
+// Task Management
+export const getTasks = () => API.get("/tasks");
+export const addTask = (task) => API.post("/tasks", task);
+export const completeTask = (taskId) => API.put(`/tasks/${taskId}/complete`);
+export const getDailyProgress = () => API.get("/progress/daily");
+
+export default API;
