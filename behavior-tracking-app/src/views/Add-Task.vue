009149fa838078d4d5833fd49eb918c485bd3a07
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-03-17 21:30:34 +0200

    updated the Add-Task view to incoporate PrimeVue

--- behavior-tracking-app/src/views/Add-Task.vue
+++ behavior-tracking-app/src/views/Add-Task.vue
@@ -1,29 +1,66 @@
 <template>
-  <div>
+  <div class="add-task">
     <h2>Add Tasks</h2>
 
-    <div v-for="(task, index) in tasks" :key="index">
-      <input v-model="task.title" placeholder="Task Title" />
-      <input v-model.number="task.points" type="number" placeholder="Points" />
-      <button @click="removeTask(index)">Remove</button>
+    <div v-for="(task, index) in tasks" :key="index" class="task-item">
+      <div class="p-field">
+        <label>Task Title</label>
+        <InputText
+          v-model="task.title"
+          placeholder="Enter task title"
+          class="p-inputtext-sm" />
+      </div>
+
+      <div class="p-field">
+        <label>Points</label>
+        <InputNumber
+          v-model="task.points"
+          :min="0"
+          :max="100"
+          class="p-inputnumber-sm" />
+      </div>
+
+      <Button
+        label="Remove"
+        icon="pi pi-trash"
+        severity="danger"
+        class="p-button-sm"
+        @click="removeTask(index)" />
     </div>
 
-    <button @click="addTask">+ Add Another Task</button>
+    <Button
+      label="+ Add Another Task"
+      icon="pi pi-plus"
+      class="p-mb-2 p-button-sm"
+      @click="addTask" />
 
-    <p v-if="totalPoints !== 100" style="color: red">
+    <Message v-if="totalPoints !== 100" severity="error" class="p-mt-2">
       Total points must be exactly 100. Current: {{ totalPoints }}
-    </p>
+    </Message>
 
-    <button @click="submitTasks" :disabled="totalPoints !== 100">
-      Submit Tasks
-    </button>
+    <Button
+      label="Submit Tasks"
+      icon="pi pi-check"
+      class="p-mt-3 p-button-sm"
+      :disabled="totalPoints !== 100"
+      @click="submitTasks" />
   </div>
 </template>
 
 <script>
 import { createTask } from "@/api.js";
+import InputText from "primevue/inputtext";
+import InputNumber from "primevue/inputnumber";
+import Button from "primevue/button";
+import Message from "primevue/message";
 
 export default {
+  components: {
+    InputText,
+    InputNumber,
+    Button,
+    Message,
+  },
   data() {
     return {
       tasks: [{ title: "", points: 0 }],
@@ -65,9 +102,11 @@ export default {
   text-align: center;
 }
 
-input {
-  display: block;
-  width: 100%;
+.task-item {
+  margin-bottom: 10px;
+}
+
+.p-field {
   margin-bottom: 10px;
 }
 </style>
