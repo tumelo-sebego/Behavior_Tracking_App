1c1632b2eba7d01829d45c3842ee23f4d81dacc0
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-04-02 23:34:02 +0200

    created the DailyProgress view

---
+++ behavior-tracking-app/src/views/DailyProgress.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header />
+    <h1>Daily Progress</h1>
+    <p>This view will display the daily progress tracking information.</p>
+    <!-- Additional content related to daily progress can be added here -->
+  </div>
+</template>
+
+<script setup>
+import Header from "../components/Header.vue";
+</script>
+
+<style scoped>
+/* Add any specific styles for the DailyProgress view here */
+</style>
