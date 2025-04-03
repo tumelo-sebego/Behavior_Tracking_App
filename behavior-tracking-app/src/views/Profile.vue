e1c2074d5c4f02e8a180a649e98241832398e3eb
sebego-tumelo <tumelosebego2.0@gmail.com>
2025-04-03 14:30:54 +0200

    created the Profile view

---
+++ behavior-tracking-app/src/views/Profile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="page-container">
+    <Header />
+    <div class="content-container">
+      <h1 class="page-title">Profile</h1>
+      <p class="page-description">
+        This is the Profile page where user information will be displayed.
+      </p>
+    </div>
+    <Navbar />
+  </div>
+</template>
+
+<script setup>
+import Header from "../components/Header.vue";
+import Navbar from "../components/Navbar.vue";
+</script>
+
+<style scoped>
+/* Mimic the layout of the home page */
+.page-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+  padding: 1rem;
+  box-sizing: border-box;
+}
+
+.content-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 2rem;
+}
+
+.page-title {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
+.page-description {
+  font-size: 1rem;
+  color: #555;
+}
+</style>
